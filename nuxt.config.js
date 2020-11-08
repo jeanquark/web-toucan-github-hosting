3241ece508c7b02b4edb08283c3ff306a7f1749c
Jean-Marc <jm.kleger@gmail.com>
2020-11-08 22:32:08 +0100

    Configured for GitHub Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -111,4 +111,7 @@ export default {
     // Build Configuration (https://go.nuxtjs.dev/config-build)
     build: {},
     ...routerBase
+    // router: {
+    //     base: '/web-toucan/'
+    //   }
 }
