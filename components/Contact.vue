4ecc3a8b53f2bd10f68cfbe184a3b5b5ef01f08f
Jean-Marc <jm.kleger@gmail.com>
2020-12-12 21:39:00 +0100

    Updated contact form with new target url

--- components/Contact.vue
+++ components/Contact.vue
@@ -74,8 +74,6 @@
 </template>
 
 <script>
-// var mailgun = require('mailgun.js')
-// var mg = mailgun.client({ username: 'api', key: process.env.NUXT_ENV_MAILGUN_PRIVATE_API_KEY, url: 'https://api.eu.mailgun.net' })
 export default {
     data() {
         return {
@@ -89,13 +87,11 @@ export default {
                 message: ''
             },
             valid: true
-            // mailgunDestinationEmailAddress: process.env.NUXT_ENV_MAILGUN_DESTINATION_EMAIL_ADDRESS
         }
     },
     computed: {},
     methods: {
         onFocus() {
-            console.log('onFocus')
             this.messageSentSuccess = false
             this.messageSentError = false
         },
@@ -107,12 +103,11 @@ export default {
         },
         async sendContactForm() {
             try {
-                console.log('sendContactForm3')
                 const valid = this.$refs.form.validate()
-                console.log('valid: ', valid)
                 if (valid) {
                     this.loading = true
-                    const res = await fetch('https://script.google.com/macros/s/AKfycbxUyOBod71w88TG7KWS66TYvlQWM2-Q8v6l0yez/exec', {
+
+                    const res = await fetch('https://script.google.com/macros/s/AKfycbyUbXcwQHeGAjSrDHbIx84PcM1FCEs-S3SnwZjHKQ/exec', {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/x-www-form-urlencoded'
@@ -136,37 +131,6 @@ export default {
                 this.loading = false
                 this.messageSentSuccess = false
                 this.messageSentError = true
-            }
-        },
-        async sendContactForm_mailgun() {
-            const valid = this.$refs.form.validate()
-            console.log('valid: ', valid)
-            if (valid) {
-                try {
-                    this.loading = true
-                    const msg = await mg.messages.create('web-toucan.com', {
-                        from: 'Contact form http://web-toucan.com',
-                        to: [process.env.NUXT_ENV_MAILGUN_DESTINATION_EMAIL_ADDRESS],
-                        subject: 'New contact form from web-toucan.com',
-                        html: `Email sent on ${new Date()}.<br /><br />
-                        Here are the information sent:<br />
-                        firstname: <b>${this.encodeHTML(this.form.firstname)}</b><br />
-                        lastname: <b>${this.encodeHTML(this.form.lastname)}</b><br />
-                        email: <b>${this.encodeHTML(this.form.email)}</b><br />
-                        message: <b>${this.encodeHTML(this.form.message)}</b><br />
-                    `
-                    })
-                    console.log('msg: ', msg)
-                    this.loading = false
-                    this.messageSentError = false
-                    this.messageSentSuccess = true
-
-                } catch (error) {
-                    console.log('error: ', error)
-                    this.loading = false
-                    this.messageSentSuccess = false
-                    this.messageSentError = true
-                }
             }
         }
     }
